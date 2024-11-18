dc973ab3bc589fc6b232440bc5141c6e08118ec9
Pjark Sander <pjark.sander@gmail.com>
2024-11-17 19:08:21 -0500

    Changed the frequency of the sin wave, and added the title "Sine Wave"

--- SineWave.py
+++ SineWave.py
@@ -6,10 +6,11 @@ def sineWave():
     #define the x axis for the plot
     x = numpy.arange(0, numpy.pi * 2, 0.01)
     #calulate corresponding y coordinates
-    y = numpy.sin(x)
+    y = numpy.sin(x*2) #frequency doubled by multiplying x by 2
 
     #plot
     plt.plot(x, y)
+    plt.title("Sine Wave")
     plt.show()
 
 if __name__ == "__main__":
